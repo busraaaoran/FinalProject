fb4f0f33d2d72883f9be585b25227de5ebdaa535
Büşra <busraoran043@gmail.com>
2021-03-08 14:53:50 +0300

    11.gün part1

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute(class ile ilgili bilgi verme ve imzalama yöntemi)
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled (gevşek bağımlılık)
+        //IoC Container (inversion of control)
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //dependency chain (bağımlılık zinciri var aşağı ilk satırda)
+            
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
